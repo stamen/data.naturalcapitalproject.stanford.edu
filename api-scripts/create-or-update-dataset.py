8c72f3f1b147cc493283587d419bf913600c6ec3
James Douglass <jdouglass@stanford.edu>
2024-03-25 17:13:36 -0700

    Adding resource from the identification URL section.

--- api-scripts/create-or-update-dataset.py
+++ api-scripts/create-or-update-dataset.py
@@ -297,6 +297,11 @@ def main(mcf_path, private=False, group=None):
             _create_resource_dict_from_file(
                 mcf_path, "YML Metadata Control File", upload=True),
         ]
+        identification_url = _get_from_mcf(mcf, 'identification.url')
+        if identification_url:
+            resources.append(
+                _create_resource_dict_from_file(
+                    mcf_path, os.path.basename(identification_url)))
         for distribution in _get_from_mcf(mcf, 'distribution').values():
             if distribution['function'].lower() == 'download':
                 try:
