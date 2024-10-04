c6ea01f24075a105e0437d2d44fa8efc7b117434
James Douglass <jdouglass@stanford.edu>
2024-10-04 13:51:47 -0700

    Adding zipfile compression method checking.

---
+++ api-scripts/check_zipfile_compression.py
@@ -0,0 +1,23 @@
+import sys
+import zipfile
+
+# integer const to string
+ZIPFILE_CONSTANTS = {}
+for attrname in dir(zipfile):
+    if attrname.startswith('ZIP_'):
+        ZIPFILE_CONSTANTS[getattr(zipfile, attrname)] = attrname
+
+
+def list_zipfile_compression(zipname):
+    with zipfile.ZipFile(zipname, 'r') as zip:
+        for info in zip.infolist():
+            print(ZIPFILE_CONSTANTS[info.compress_type] + "\t",
+                  info.filename)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python check_zipfile_compression.py zipfile")
+        sys.exit(1)
+
+    list_zipfile_compression(sys.argv[1])
