06631997d38ac485bd9718221b69af3f8dfb47f1
Eric Brelsford <ebrelsford@gmail.com>
2024-10-25 20:37:29 +0000

    Update zipexpand with sources from metadata

--- api-scripts/sync-datasets.py
+++ api-scripts/sync-datasets.py
@@ -1,42 +1,85 @@
 #!/usr/bin/env python
 import json
+import os
 import requests
+import yaml
 
-DST_APIKEY = 'APIKEY'
-SRC = 'https://data.naturalcapitalproject.stanford.edu'
-DST = "http://localhost:5000"
+SRC = os.environ.get('SYNC_SRC_URL', 'https://data.naturalcapitalproject.stanford.edu')
+DST = os.environ.get('SYNC_DST_URL', 'http://localhost:5000')
+DST_APIKEY = os.environ['SYNC_DST_CKAN_APIKEY']
 
-list_response = requests.get(SRC + '/api/3/action/package_list')
 
-for id in list_response.json()['result']:
-    print(id)
-    package_response = requests.get(SRC + '/api/3/action/package_show?id=' + id)
+def get_dataset_metadata(dataset):
+    if dataset['resources']:
+        for resource in dataset['resources']:
+            if resource['description'] == 'Geometamaker YML':
+                r = requests.get(resource['url'])
+                return yaml.safe_load(r.text)
+    return None
 
-    organization_id = None
-    organization_response = requests.get(DST + '/api/action/organization_show?id=' + package_response.json()['result']['owner_org'])
-    print(organization_response.json()['result'])
 
-    if organization_response.status_code == 404:
-        print('Creating org')
-        organization_post_response = requests.post(
-            DST + '/api/action/organization_create',
-            headers={'Authorization': DST_APIKEY},
-            json=package_response.json()['result']['organization']
+def get_dataset_sources(dataset_metadata):
+    return dataset_metadata.get('sources', None)
+
+
+def delete_datasets(dst, dst_apikey):
+    list_response = requests.get(dst + '/api/3/action/package_list')
+
+    for id in list_response.json()['result']:
+        print('Deleting ' + id)
+        delete_response = requests.post(dst + '/api/action/package_delete',
+                                        json={'id': id},
+                                        headers={'Authorization': dst_apikey})
+        purge_response = requests.post(dst + '/api/action/dataset_purge',
+                                        json={'id': id},
+                                        headers={'Authorization': dst_apikey})
+
+
+def sync_datasets(src, dst, dst_apikey):
+    list_response = requests.get(src + '/api/3/action/package_list')
+
+    for id in list_response.json()['result']:
+        print('Adding ' + id)
+        package_response = requests.get(src + '/api/3/action/package_show?id=' + id)
+
+        organization_id = None
+        organization_response = requests.get(dst + '/api/action/organization_show?id=' + package_response.json()['result']['owner_org'])
+
+        if organization_response.status_code == 404:
+            print('Creating org')
+            organization_post_response = requests.post(
+                dst + '/api/action/organization_create',
+                headers={'Authorization': dst_apikey},
+                json=package_response.json()['result']['organization']
+            )
+            organization_id = organization_post_response.json()['result']['id']
+
+        package = package_response.json()['result']
+        package['extras'] = [] # XXX skipping extras for now
+
+        # If dataset has metadata with sources in it, add those
+        metadata = get_dataset_metadata(package)
+        sources = get_dataset_sources(metadata)
+        if sources:
+            # TODO maybe better on the resource itself?
+            package['extras'].append({'key': 'sources', 'value': json.dumps(sources)})
+
+        post_response = requests.post(
+            dst + '/api/action/package_create',
+            headers={'Authorization': dst_apikey},
+            json=package
         )
-        organization_id = organization_post_response.json()['result']['id']
 
-    package = package_response.json()['result']
-    package['extras'] = [] # XXX skipping extras for now
+        if (post_response.status_code != 200):
+            print(post_response.json()['error'])
+            break
 
-    post_response = requests.post(
-        DST + '/api/action/package_create',
-        headers={'Authorization': DST_APIKEY},
-        json=package
-    )
 
-    print(post_response.status_code)
-    if (post_response.status_code != 200):
-        print(post_response.json()['error'])
-        break
+if __name__ == '__main__':
+    print('Deleting existing datasets...')
+    delete_datasets(DST, DST_APIKEY)
+    print('Done.')
 
-    print(post_response.text)
+    print('Syncing datasets...')
+    sync_datasets(SRC, DST, DST_APIKEY)
+    print('Done.')
--- src/ckanext-zipexpand/ckanext/zipexpand/plugin.py
+++ src/ckanext-zipexpand/ckanext/zipexpand/plugin.py
@@ -1,16 +1,42 @@
+import json
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
+def parse_sources(sources):
+    sources_arr = sorted(json.loads(sources))
+    output_arr = []
+
+    for s in sources_arr:
+        components = s.split('\\')
+        current_dir = None
+        dir_options = output_arr
+
+        for component in components[:-1]:
+            current_dir = next((x for x in dir_options if x['name'] == component), None)
+            if not current_dir:
+                current_dir = { 'name': component, 'type': 'directory', 'children': [] }
+                dir_options.append(current_dir)
+
+            dir_options = current_dir['children']
+            
+        dir_options.append({
+            'name': components[-1],
+            'type': 'file',
+            'extension': components[-1].split('.')[-1]
+            })
+
+    return output_arr
+
+
 class ZipexpandPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
-    
-
-    # IConfigurer
+    plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "zipexpand")
 
-    
+    def get_helpers(self):
+        return {'zipexpand_parse_sources': parse_sources}
