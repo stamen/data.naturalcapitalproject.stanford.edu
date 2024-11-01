38a3fd7499046b454bb79233f9637f3f05751fc3
Eric Brelsford <ebrelsford@gmail.com>
2024-11-01 19:57:01 +0000

    Add collapse

---
+++ src/ckanext-natcap/ckanext/natcap/assets/js/natcap-facet-collapse.js
@@ -0,0 +1,18 @@
+ckan.module("natcap-facet-collapse", function ($, _) {
+  "use strict";
+  return {
+    options: {
+      debug: false,
+    },
+
+    initialize: function () {
+      const header = $(this.el).find('.search-filters-section-header');
+      const content = $(this.el).find('.search-filters-section-content');
+
+      header.click(() => {
+        content.toggle();
+        header.toggleClass('collapsed');
+      });
+    },
+  };
+});
