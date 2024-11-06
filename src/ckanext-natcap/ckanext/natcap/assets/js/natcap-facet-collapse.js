980f3f42c4af99a0ee56e4958abf44a77b793e65
Eric Brelsford <ebrelsford@gmail.com>
2024-11-06 04:38:59 +0000

    Keep content open if any active

--- src/ckanext-natcap/ckanext/natcap/assets/js/natcap-facet-collapse.js
+++ src/ckanext-natcap/ckanext/natcap/assets/js/natcap-facet-collapse.js
@@ -7,12 +7,10 @@ ckan.module("natcap-facet-collapse", function ($, _) {
 
     initialize: function () {
       const header = $(this.el).find('.search-filters-section-header');
-      const content = $(this.el).find('.search-filters-section-content');
+      const root = $(this.el);
 
-      // TODO open if any active
       header.click(() => {
-        content.toggle();
-        header.toggleClass('collapsed');
+        root.toggleClass('collapsed');
       });
     },
   };
