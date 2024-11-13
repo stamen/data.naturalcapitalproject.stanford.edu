e334865f53f850afac92b829bd058ba246275499
Eric Brelsford <ebrelsford@gmail.com>
2024-11-13 16:43:54 +0000

    Add js code to copy

--- src/ckanext-mappreview/ckanext/mappreview/plugin.py
+++ src/ckanext-mappreview/ckanext/mappreview/plugin.py
@@ -1,4 +1,5 @@
 import json
+from urllib.parse import urlencode
 from ckan.common import config
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
@@ -21,6 +22,41 @@ def parse_metadata(pkg):
     return json.loads(get_mappreview(pkg)['value'])
 
 
+def get_layer_js(layer, config):
+    titiler_url = get_config()['titiler_url']
+    query_params = {
+        'colormap_name': 'viridis',
+        'bidx': 1,
+        'url': layer['url'],
+        'rescale': f'{layer["pixel_percentile_2"]},{layer["pixel_percentile_98"]}',
+    }
+    layer_url = titiler_url + "/cog/tiles/WebMercatorQuad/{z}/{x}/{y}@2x?" + urlencode(query_params)
+    return f"L.tileLayer('{layer_url}').addTo(map);"
+
+
+def get_layers_js(pkg):
+    layers = parse_metadata(pkg)['layers']
+    return '\n'.join([get_layer_js(layer, config) for layer in layers])
+
+
+def generate_map_code(pkg):
+    return """
+<div id="map" style="width: 100%; height: 400px;"></div>
+
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+<script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+<script>
+  var map = L.map('map').setView([0, 0], 4);
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+""" + get_layers_js(pkg) + """
+</script>
+"""
+
+
 class MappreviewPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
@@ -35,4 +71,5 @@ class MappreviewPlugin(plugins.SingletonPlugin):
             'mappreview_get_config': get_config,
             'mappreview_parse_metadata': parse_metadata,
             'mappreview_should_show': should_show,
+            'mappreview_generate_map_code': generate_map_code,
         }
